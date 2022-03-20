b17c0ee5812e8e62f276fe316da20636cf717f0a
AWhiteElephant <AWhiteElephant@123.com>
2022-03-21 00:02:44 +0800

    first commit

---
+++ src/main/java/com/foodtok/FoodtokApplication.java
@@ -0,0 +1,13 @@
+package com.foodtok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodtokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodtok/FoodtokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodtok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
